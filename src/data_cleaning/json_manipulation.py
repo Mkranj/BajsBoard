1ffa91001432fd4b6b5fb2607fcb25d4150e6dda
mkranjcev <kranjcev@srce.hr>
2025-11-13 15:21:10 +0100

    bind bike id's to stations at certain times

--- src/data_cleaning/json_manipulation.py
+++ src/data_cleaning/json_manipulation.py
@@ -1,6 +1,7 @@
 # Functions for working with raw JSON data and transforming it to formats easier to use in further analysis
 
 import pandas as pd
+from datetime import datetime
 
 def trim_json_to_locations(json: dict) -> list:
     '''
@@ -40,6 +41,35 @@ def gather_bajs_locations(loc_list: list[dict]) -> pd.DataFrame:
         "lat": loc_lat,
         "lng": loc_lng,
         "no_racks": loc_racks
+    })
+
+    return df
+
+def get_bikes_in_stations(time: datetime, loc_list: list[dict]) -> pd.DataFrame:
+    '''
+    Create a dataframe of bike IDS at a certain station at a certain time
+    
+    Takes a list of full location data from `trim_json_to_locations`.
+    Each row has the datetime info, so multiple such df's can be combined while retaining all relevant information.
+    
+    Args:
+        time: datetime describing when the info was gathered
+        loc_list: A list of Bajs racks locations, each a dict with name, location, bike info etc.
+
+    Returns: dataframe each row being a Bajs location (identified by uid) at a certain time, with list of individual bike IDs at that station
+    '''
+
+    locations = []
+    bike_ids = []
+
+    for location in loc_list:
+        locations.append(location["uid"])
+        bike_ids.append(location["bike_numbers"])
+
+    df = pd.DataFrame({
+        "uid": locations,
+        "time": time, # same for all rows
+        "bikes_at_station": bike_ids
     })
 
     return df
