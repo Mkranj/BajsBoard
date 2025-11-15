859e57de31beea13ad38f4a671089932ed7cfd1b
Mkranj <mkranjcev6@gmail.com>
2025-11-15 10:30:38 +0100

    bin inside mapping fn, no need to change df

--- src/visualization/station_map.py
+++ src/visualization/station_map.py
@@ -18,13 +18,16 @@ def populate_map_with_stations(map: folium.Map,
     Returns: map with added Circles
 
     '''
+    if metric_size is not None:
+        binned_metric = pd.qcut(stations_df[metric_size], 10, list(range(1, 10 + 1)))
+        binned_metric = binned_metric.astype("float")
 
     for ix in range(stations_df.shape[0]):
 
         if metric_size is not None:
             tooltip_text = stations_df["name"].iloc[ix] + "<br>" + metric_tooltip_name + str(stations_df[metric_size].iloc[ix])
             # Radius depends on the metric
-            custom_radius = stations_df[metric_size].iloc[ix] * 5 + stations_df[metric_size].iloc[ix]
+            custom_radius = binned_metric.iloc[ix] * 5
         else:
             tooltip_text = stations_df["name"].iloc[ix]
             custom_radius = 5
