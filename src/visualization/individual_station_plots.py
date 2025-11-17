8c295403b3020a20fab64f93360b7dc42d70c3bf
mkranjcev <kranjcev@srce.hr>
2025-11-17 10:46:31 +0100

    add fn for plotting changes at a station

---
+++ src/visualization/individual_station_plots.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib as plt
+
+def plot_hourly_bar(station_data: pd.DataFrame, changes_type: str):
+    if changes_type == "total":
+        columns = ["changes"]
+        stack = False
+    elif changes_type == "in/out":
+        columns = ["incoming", "outgoing"]
+        stack = True
+
+    chart = station_data.plot(kind = "bar",
+        y = columns, 
+        stacked = stack, 
+        legend = "reverse")
+
+    chart.tick_params(axis = "x", labelrotation=0)
+
+    return chart
