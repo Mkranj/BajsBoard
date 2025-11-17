c79d578270aac609b71db4bd22207ac7497df7b1
mkranjcev <kranjcev@srce.hr>
2025-11-17 11:29:37 +0100

    modify weekday plot appearance

--- src/visualization/individual_station_plots.py
+++ src/visualization/individual_station_plots.py
@@ -2,7 +2,7 @@ import pandas as pd
 import seaborn as sns
 import matplotlib as plt
 
-def plot_hourly_bar(station_data: pd.DataFrame, changes_type: str):
+def plot_hourly_bar(station_data: pd.DataFrame, changes_type: str) -> plt.axes:
     if changes_type == "total":
         columns = ["changes"]
         stack = False
@@ -19,20 +19,16 @@ def plot_hourly_bar(station_data: pd.DataFrame, changes_type: str):
 
     return chart
 
-def plot_weekday_bar(station_data: pd.DataFrame, changes_type: str):
-    # TODO - without in out?
-    if changes_type == "total":
-        columns = ["changes"]
-        stack = False
-    elif changes_type == "in/out":
-        columns = ["incoming", "outgoing"]
-        stack = True
-
+def plot_weekday_bar(station_data: pd.DataFrame) -> plt.axes:
+    columns = ["changes"]
+    
     chart = station_data.plot(kind = "bar",
         y = columns, 
-        stacked = stack, 
-        legend = "reverse")
+        stacked = False,
+        legend = False)
 
     chart.tick_params(axis = "x", labelrotation=0)
+    chart.set_xticklabels(["ponedjeljak", "utorak", "srijeda", "četvrtak", "petak", "subota", "nedjelja"])
+    chart.set_xlabel(None)
 
     return chart
