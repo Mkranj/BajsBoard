c091ef6b5c98d02c522375666e233d4b7f2dc58f
mkranjcev <kranjcev@srce.hr>
2025-11-17 13:41:50 +0100

    combined plot needs two dataframes

--- src/visualization/individual_station_plots.py
+++ src/visualization/individual_station_plots.py
@@ -25,7 +25,7 @@ def plot_hourly_bar(station_data: pd.DataFrame, changes_type: str, ax: Optional[
         legend = "reverse",
         ax=ax)
 
-    ax.tick_params(axis = "x", labelrotation=0)
+    ax.tick_params(axis = "x", labelrotation=0, labelsize=7)
 
     return ax
 
@@ -41,17 +41,17 @@ def plot_weekday_bar(station_data: pd.DataFrame, ax: Optional[Axes] = None) -> A
         legend = False,
         ax = ax)
 
-    ax.tick_params(axis = "x", labelrotation=0)
+    ax.tick_params(axis = "x", labelrotation=0, labelsize=7)
     ax.set_xticklabels(["ponedjeljak", "utorak", "srijeda", "četvrtak", "petak", "subota", "nedjelja"])
     ax.set_xlabel(None)
 
     return ax
 
-def combined_plot_daily_weekly(station_data: pd.DataFrame) -> (Figure, Axes):
-    fig, ax = plt.subplots(nrows = 1, ncols = 2, figsize = (10, 3))
+def combined_plot_daily_weekly(station_data_hourly: pd.DataFrame, station_data_daily: pd.DataFrame) -> (Figure, Axes):
+    fig, ax = plt.subplots(nrows = 1, ncols = 2, figsize = (12, 4))
 
-    _ = plot_hourly_bar(station_data, changes_type = "in/out", ax = ax[0])
-    _ = plot_weekday_bar(station_data, ax = ax[1])
+    _ = plot_hourly_bar(station_data_hourly, changes_type = "in/out", ax = ax[0])
+    _ = plot_weekday_bar(station_data_daily, ax = ax[1])
     
     plt.close(fig)
     return fig, ax
