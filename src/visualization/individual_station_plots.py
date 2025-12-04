315bd5f31ad20d160082957e21624dc0b19105ba
mkranjcev <kranjcev@srce.hr>
2025-12-04 14:42:03 +0100

    preimenuj fn za plot changes po satima

--- src/visualization/individual_station_plots.py
+++ src/visualization/individual_station_plots.py
@@ -8,7 +8,7 @@ from matplotlib.figure import Figure
 import base64
 from io import BytesIO
 
-def plot_hourly_bar(station_data: pd.DataFrame, changes_type: str, ax: Optional[Axes] = None) -> Axes:
+def plot_hourly_changes_bar(station_data: pd.DataFrame, changes_type: str, ax: Optional[Axes] = None) -> Axes:
     if ax is None:
         fig, ax = plt.subplots()
     
@@ -59,7 +59,7 @@ def plot_weekday_bar(station_data: pd.DataFrame, ax: Optional[Axes] = None) -> A
 def combined_plot_daily_weekly(station_data_hourly: pd.DataFrame, station_data_daily: pd.DataFrame) -> (Figure, Axes):
     fig, ax = plt.subplots(nrows = 1, ncols = 2, figsize = (12, 4))
 
-    _ = plot_hourly_bar(station_data_hourly, changes_type = "in/out", ax = ax[0])
+    _ = plot_hourly_changes_bar(station_data_hourly, changes_type = "in/out", ax = ax[0])
     _ = plot_weekday_bar(station_data_daily, ax = ax[1])
     
     plt.close(fig)
